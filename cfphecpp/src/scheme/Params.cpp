4b207b07999239dbd0404b5b63c5184cd5616e89
kimandrik <kimandrik@snu.ac.kr>
2016-09-13 17:18:47 +0900

    kimandrik: parameters changes

--- cfphecpp/src/scheme/Params.cpp
+++ cfphecpp/src/scheme/Params.cpp
@@ -13,8 +13,8 @@ Params::Params(long lambda) : lambda(lambda) {
 	L = 10;
 	sigma = 3;
 	rho = 0.5;
-	d = 1 << 14;
-	n = 1 << 13;
+	d = 1 << 15;
+	n = 1 << 14;
 	h = 64;
 	logp = 30;
 	logq = logp * L;
@@ -22,7 +22,9 @@ Params::Params(long lambda) : lambda(lambda) {
 	logPq = logq + logP;
 
 	Bclean = (8.0 * sqrt(2.0) * sigma * n) + (6.0 * sigma * sqrt(n)) + (16.0 * sigma * sqrt(h * n * 1.0));
+
 	Bscale = sqrt(3.0 * n) + 8 * sqrt(h * n / 3.0);
+
 	for (long l = 0; l < L; ++l) {
 		ZZ Bkstemp;
 		Bkstemp = 8 << (logp * (l+1));
