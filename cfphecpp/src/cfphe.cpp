b58ab0b808c44a4cc109d4eb4bce15a64aa932d0
kimandrik <kimandrik@169.254.117.225>
2017-05-19 00:32:43 +0900

    kimandrik: VERSION 1.1

--- cfphecpp/src/cfphe.cpp
+++ cfphecpp/src/cfphe.cpp
@@ -33,16 +33,16 @@ int main() {
 
 	/*
 	 * Params: logN, logl, logp, L, logSlots
-	 * Suggested: 13, 2, 30, 5, 12
+	 * Suggested: 13, 2, 30, 5, 3
 	 */
-//	TestScheme::testEncode(13, 2, 30, 4, 12);
+//	TestScheme::testEncodeBatch(13, 2, 30, 4, 3);
 
 	//-----------------------------------------
 
 	/*
 	 * Params: logN, logl, logp, L, logDegree, logSlots
 	 * Suggested: 13, 2, 30, 5, 4, 3
-	 * Suggested: 15, 2, 56, 11, 10
+	 * Suggested: 15, 2, 56, 11, 10, 3
 	 */
 
 //	TestScheme::testPowerOf2Batch(13, 2, 30, 5, 4, 3);
@@ -53,11 +53,11 @@ int main() {
 	/*
 	 * Params: logN, logl, logp, L, degree, logSlots
 	 * Suggested: 13, 2, 30, 5, 13, 3
-	 * Suggested: 15, 2, 56, 11, 903
+	 * Suggested: 15, 2, 56, 11, 903, 3
 	 */
 
 //	TestScheme::testPowerBatch(13, 4, 30, 5, 13, 3);
-//	TestScheme::testPowerExtended(13, 30, 5, 13);
+//	TestScheme::testPowerExtended(13, 4, 30, 5, 13);
 
 	//-----------------------------------------
 
@@ -73,7 +73,7 @@ int main() {
 	/*
 	 * Params: logN, logl, logp, L, invSteps, logSlots
 	 * Suggested: 13, 3, 25, 10, 8, 3
-	 * Suggested: 14, 3, 32, 10, 8
+	 * Suggested: 14, 3, 32, 10, 8, 3
 	 */
 
 //	TestScheme::testInverseBatch(13, 3, 25, 10, 8, 3);
@@ -83,7 +83,7 @@ int main() {
 	/*
 	 * Params: logN, logl, logp, L, expSteps, logSlots
 	 * Suggested: 13, 2, 35, 5, 8, 3
-	 * Suggested: 13, 37, 35, 4, 8, 3 (for simple)
+	 * Suggested: 13, 37, 35, 4, 8, 3 (for lazy)
 	 */
 //	TestScheme::testExponentBatch(13, 2, 35, 5, 8, 3);
 //	TestScheme::testLazyExponentBatch(13, 37, 35, 4, 8, 3);
@@ -94,7 +94,7 @@ int main() {
 	/*
 	 * Params: logN, logl, logp, L, sigmoidSteps, logSlots
 	 * Suggested: 13, 1, 35, 5, 7, 3
-	 * Suggested: 13, 36, 35, 4, 7, 3 (for simple)
+	 * Suggested: 13, 36, 35, 4, 7, 3 (for lazy)
 	 */
 
 //	TestScheme::testSigmoidBatch(13, 1, 35, 5, 7, 3);
@@ -106,7 +106,7 @@ int main() {
 	/*
 	 * Params: logN, logp, L, logFFTdim, FFTdeg
 	 * Suggested: 14, 7, 50, 3, 4, 5
-	 * Suggested: 14, 7, 50, 2, 4, 5 (for simple)
+	 * Suggested: 14, 7, 50, 2, 4, 5 (for lazy)
 	 */
 //	TestScheme::testFFT(14, 1, 30, 3, 13, 5);
 //	TestScheme::testLazyFFT(13, 7, 50, 2, 4, 5);
