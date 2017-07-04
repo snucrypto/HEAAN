06237193de9564159e4fe682ec5423871a89fef0
kimandrik <kimandrik@10.20.24.118>
2017-07-04 15:51:20 +0900

    kimandrik: main file

---
+++ HEAC/src/test/heac.cpp
@@ -0,0 +1,146 @@
+#include "../Params.h"
+#include "../PubKey.h"
+#include "../Scheme.h"
+#include "../SchemeAlgo.h"
+#include "../SchemeAux.h"
+#include "../SecKey.h"
+#include "../TestScheme.h"
+#include "../TimeUtils.h"
+
+void test() {
+	//-----------------------------------------
+	TimeUtils timeutils;
+	long logN = 13;
+	long logl = 3;
+	long logp = 30;
+	long L = 2;
+	Params params(logN, logl, logp, L);
+	SecKey secretKey(params);
+	PubKey publicKey(params, secretKey);
+	SchemeAux schemeaux(logp, logN + 2);
+	Scheme scheme(params, secretKey, publicKey, schemeaux);
+	SchemeAlgo algo(scheme);
+	//-----------------------------------------
+}
+
+int main() {
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, logSlots
+	 * Suggested: 13, 2, 30, 5, 3
+	 */
+
+	TestScheme::testEncodeBatch(13, 5, 30, 4, 3);
+
+	/*
+	 * Params: logN, logl, logp, L, logSlots
+	 * Suggested: 13, 2, 30, 5, 3
+	 */
+
+	TestScheme::testConjugate(13, 5, 30, 4, 3);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, rotlogSlots, logSlots
+	 * Suggested: 13, 2, 30, 5, 0, 3
+	 */
+
+//	TestScheme::testRotate2(13, 2, 30, 4, 2, 3);
+//	TestScheme::testRotate(13, 2, 30, 4, 17, 3);
+//	TestScheme::testSlotssum(13, 2, 30, 4, 12);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, logDegree, logSlots
+	 * Suggested: 13, 2, 30, 5, 4, 3
+	 * Suggested: 15, 2, 56, 11, 10, 3
+	 */
+
+//	TestScheme::testPowerOf2Batch(13, 2, 30, 5, 4, 3);
+//	TestScheme::testPowerOf2Extended(15, 3, 56, 11, 10);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, degree, logSlots
+	 * Suggested: 13, 2, 30, 5, 13, 3
+	 * Suggested: 15, 2, 56, 11, 903, 3
+	 */
+
+//	TestScheme::testPowerBatch(15, 4, 56, 11, 1024, 3);
+//	TestScheme::testPowerExtended(13, 4, 30, 5, 13);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, logDegree, logSlots
+	 * Suggested: 13, 2, 30, 5, 4, 3
+	 * Suggested: 15, 2, 56, 11, 10, 3
+	 */
+
+//	TestScheme::testProd2Batch(13, 4, 30, 5, 4, 3);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, invSteps, logSlots
+	 * Suggested: 13, 3, 25, 10, 8, 3
+	 * Suggested: 14, 3, 32, 10, 8, 3
+	 */
+
+//	TestScheme::testInverseBatch(13, 3, 25, 10, 8, 3);
+//	TestScheme::testInverseExtended(13, 3, 25, 10, 8);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, degree, logSlots
+	 * Suggested: 13, 2, 35, 5, 8, 3
+	 * Suggested: 13, 37, 35, 4, 8, 3 (for lazy)
+	 */
+
+//	TestScheme::testLogarithmBatch(13, 4, 35, 6, 10, 3);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, degree, logSlots
+	 * Suggested: 13, 2, 35, 5, 8, 3
+	 * Suggested: 13, 37, 35, 4, 8, 3 (for lazy)
+	 */
+
+//	TestScheme::testExponentBatch(19, 4, 35, 50, 7, 3); // less than 1 minute
+//	TestScheme::testExponentBatch(13, 4, 35, 6, 10, 3);
+//	TestScheme::testExponentBatchLazy(13, 37, 35, 4, 8, 3);
+//	TestScheme::testExponentExtended(13, 2, 25, 6, 10);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, degree, logSlots
+	 * Suggested: 13, 1, 35, 5, 7, 3
+	 * Suggested: 13, 36, 35, 4, 7, 3 (for lazy)
+	 */
+
+//	TestScheme::testSigmoidBatch(13, 2, 35, 5, 7, 3);
+//	TestScheme::testSigmoidBatchLazy(13, 36, 35, 4, 7, 3);
+//	TestScheme::testSigmoidExtended(13, 1, 35, 5, 7);
+
+	//-----------------------------------------
+
+	/*
+	 * Params: logN, logl, logp, L, logfftdim, logSlots
+	 * Suggested: 14, 7, 50, 3, 4, 3;
+	 * Suggested: 14, 57, 50, 2, 4 (for lazy)
+	 */
+
+//	TestScheme::testFFT(13, 5, 50, 3, 4);
+//	TestScheme::testFFTBatch(13, 7, 50, 4, 4, 3);
+//	TestScheme::testFFTLazy(13, 51, 50, 2, 4);
+
+	return 0;
+}
