8c9dfeb350f7d178d73f869e6f60c57d140769a1
kimandrik <kimandrik@192.168.0.34>
2017-09-18 19:21:48 +0900

    kimandrik: use 5 as a primitive root

--- HEAAN/src/Params.cpp
+++ HEAAN/src/Params.cpp
@@ -16,7 +16,7 @@ Params::Params(long logN, long logq, double sigma, long h) :
 	long N2 = N << 1;
 	for (long i = 0; i < N / 2; ++i) {
 		rotGroup[i] = val;
-		val *= 3;
+		val *= 5;
 		val %= N2;
 	}
 }
