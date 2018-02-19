8978e6468af27e428ed91f1d57c3092c13e1c416
kimandrik <kimandrik@192.168.0.34>
2018-02-19 18:34:55 +0900

    kimandrik: lib and run folders change

--- HEAAN/lib/sources.mk
+++ HEAAN/lib/sources.mk
@@ -4,8 +4,8 @@
 
 C_UPPER_SRCS := 
 CXX_SRCS := 
-OBJ_SRCS := 
 C++_SRCS := 
+OBJ_SRCS := 
 CC_SRCS := 
 ASM_SRCS := 
 CPP_SRCS := 
@@ -14,7 +14,7 @@ O_SRCS :=
 S_UPPER_SRCS := 
 CC_DEPS := 
 C++_DEPS := 
-ARCHIVES := 
+EXECUTABLES := 
 C_UPPER_DEPS := 
 CXX_DEPS := 
 OBJS := 
--- HEAAN/run/src/subdir.mk
+++ HEAAN/run/src/subdir.mk
@@ -4,55 +4,52 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/CZZ.cpp \
 ../src/Ciphertext.cpp \
 ../src/Context.cpp \
 ../src/EvaluatorUtils.cpp \
 ../src/HEAAN.cpp \
 ../src/Key.cpp \
 ../src/NumUtils.cpp \
-../src/Params.cpp \
 ../src/Plaintext.cpp \
 ../src/Ring2Utils.cpp \
 ../src/Scheme.cpp \
 ../src/SchemeAlgo.cpp \
 ../src/SecretKey.cpp \
+../src/SerializationUtils.cpp \
 ../src/StringUtils.cpp \
 ../src/TestScheme.cpp \
 ../src/TimeUtils.cpp 
 
 OBJS += \
-./src/CZZ.o \
 ./src/Ciphertext.o \
 ./src/Context.o \
 ./src/EvaluatorUtils.o \
 ./src/HEAAN.o \
 ./src/Key.o \
 ./src/NumUtils.o \
-./src/Params.o \
 ./src/Plaintext.o \
 ./src/Ring2Utils.o \
 ./src/Scheme.o \
 ./src/SchemeAlgo.o \
 ./src/SecretKey.o \
+./src/SerializationUtils.o \
 ./src/StringUtils.o \
 ./src/TestScheme.o \
 ./src/TimeUtils.o 
 
 CPP_DEPS += \
-./src/CZZ.d \
 ./src/Ciphertext.d \
 ./src/Context.d \
 ./src/EvaluatorUtils.d \
 ./src/HEAAN.d \
 ./src/Key.d \
 ./src/NumUtils.d \
-./src/Params.d \
 ./src/Plaintext.d \
 ./src/Ring2Utils.d \
 ./src/Scheme.d \
 ./src/SchemeAlgo.d \
 ./src/SecretKey.d \
+./src/SerializationUtils.d \
 ./src/StringUtils.d \
 ./src/TestScheme.d \
 ./src/TimeUtils.d 
@@ -62,7 +59,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/local/include -O2 -g -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I/usr/local/include -O3 -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
